95d7660c62321d4f0795c1cc7623524884e115fb
lassehj0 <lassehj00@github.com>
2022-12-07 12:37:48 +0100

    ad

--- ModelsApi/frontend/src/pages/loginPage.jsx
+++ ModelsApi/frontend/src/pages/loginPage.jsx
@@ -1,9 +1,10 @@
-export default Login = () => {
+export default function Login() {
 	return (
 		<div id='page'>
 			<h1>Login</h1>
-			<textarea id='username' rows='1' />
-			<textarea id='password' rows='1' />
+			<textarea id='username' className='signInText' rows='1' />
+			<textarea id='password' className='signInText' rows='1' />
+			<button id='login'>Sign in</button>
 		</div>
 	);
-};
+}
