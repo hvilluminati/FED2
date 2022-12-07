5ef4f5a0d8fa60e0540cb7063fefd7fcc1763a08
oscarboe <Oscarboen@gmail.com>
2022-12-07 14:11:28 +0100

    ttried to route

--- ModelsApi/frontend/src/App.js
+++ ModelsApi/frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Login from './pages/loginPage';
 import Welcome from './pages/welcomePage';
 import Jobs from './pages/jobListPage';
+import AddModelToJob from './pages/AddModelToJob';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
@@ -14,6 +15,7 @@ function App() {
 				<Route path='/' element={<Login func={setType} />} />
 				<Route path='welcome' element={<Welcome />} />
 				<Route path='jobs' element={<Jobs />} />
+				<Route path='AddModelToJob' element{<AddModelToJob />}></Route>
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ ModelsApi/frontend/src/pages/AddModelToJob.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import { axiosAddModelToJob } from '../axioscalls';
+import { useNavigate } from 'react-router-dom';
+
+export default function AddModelToJob() {
+  const [modelId, setModelId] = useState();
+  const [jobId, setJobId] = useState();
+
+  function AddModelToJob() {
+    axiosAddModelToJob(modelId, jobId).then((resp) => {
+      console.log(resp);
+    });
+
+    console.log(jobId, modelId);
+  }
+
+  return (
+    <div id='page'>
+      <h1>Add Model to job</h1>
+      <textarea
+        id='ModelID'
+        className='signInText'
+        rows='1'
+        onChange={(event) => setModelId(event.target.value)}
+      />
+      <textarea
+        id='JobID'
+        className='signInText'
+        rows='1'
+        onChange={(event) => setJobId(event.target.value)}
+      />
+      <button id='Add' onClick={AddModelToJob}>
+        Add Model
+      </button>
+    </div>
+  );
+}
