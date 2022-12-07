ab59aad08c8685d09ed8e0c718c0fe1b054f1c00
hvilluminati <Hvillum.magnus@gmail.com>
2022-12-07 16:22:28 +0100

    AddManager works maybe

--- ModelsApi/frontend/src/App.js
+++ ModelsApi/frontend/src/App.js
@@ -1,19 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect} from 'react';
 import Login from './pages/loginPage';
 import Welcome from './pages/welcomePage';
-import Jobs from './pages/jobListPage';
-import AddExpense from './pages/AddExpensePage';
-import ChangeModelOnJob from './pages/ChangeModelOnJob';
-import CreateJob from './pages/CreateJob';
+import Jobs from './pages/JobListPage';
+import AddModelToJob from './pages/ChangeModelOnJob';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { setBaseUrl } from './Services/api';
-import { AddManager } from './pages/addManager';
+import { useState } from 'react';
+import AddManager from './pages/AddManager';
 import jwtDecode from 'jwt-decode';
 
 function App() {
-	setBaseUrl('https://localhost:7181/api/');
-
 	const [type, setType] = useState('none');
 
 	useEffect(() => {
@@ -30,10 +26,8 @@ function App() {
 				{localStorage.getItem('type') !== 'none' && (
 					<Route path='welcome' element={<Welcome />} />
 				)}
-				{<Route path='expense' element={<AddExpense />} />}
-				<Route path='/addmanager' element={<AddManager />} />
-				<Route path='ChangeModelOnJob' element={<ChangeModelOnJob />} />
-				<Route path='CreateJob' element={<CreateJob />} />
+				{/* <Route path='jobs' element={<Jobs />} /> */}
+				<Route path="/addmanager" element={<AddManager />} />
 			</Routes>
 		</BrowserRouter>
 	);
