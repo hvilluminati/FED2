4b483a98024b941e5c7a238847f73f0acfccbe89
lassehj0 <lassehj00@github.com>
2022-12-07 15:45:59 +0100

    sdf

--- ModelsApi/frontend/src/App.js
+++ ModelsApi/frontend/src/App.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Login from './pages/loginPage';
 import Welcome from './pages/welcomePage';
 import Jobs from './pages/jobListPage';
 import AddModelToJob from './pages/AddModelToJob';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { useState } from 'react';
 import jwtDecode from 'jwt-decode';
 
 function App() {
@@ -20,10 +19,9 @@ function App() {
 		<BrowserRouter>
 			<Routes>
 				<Route path='/' element={<Login />} />
-				{localStorage.getItem('type') !== 'none' && (
-					<Route path='welcome' element={<Welcome />} />
-				)}
-				{/* <Route path='jobs' element={<Jobs />} /> */}
+				<Route path='welcome' element={<Welcome />} />
+				<Route path='jobs' element={<Jobs />} />
+				<Route path='addModel' element={<AddModelToJob />} />
 			</Routes>
 		</BrowserRouter>
 	);
