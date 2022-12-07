605237218545ad9f89815dded59bcd324f60ee9b
oscarboe <Oscarboen@gmail.com>
2022-12-07 15:46:18 +0100

    changed addmodeltojob to changemodelonjob and added delete functionality to it.
    added createJob

--- ModelsApi/frontend/src/App.js
+++ ModelsApi/frontend/src/App.js
@@ -2,31 +2,34 @@ import React, { useEffect } from 'react';
 import Login from './pages/loginPage';
 import Welcome from './pages/welcomePage';
 import Jobs from './pages/jobListPage';
-import AddModelToJob from './pages/AddModelToJob';
+import ChangeModelOnJob from './pages/ChangeModelOnJob';
+import CreateJob from './pages/CreateJob';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import jwtDecode from 'jwt-decode';
 
 function App() {
-	useEffect(() => {
-		var jwt = localStorage.getItem('jwt');
-		if (jwt !== null) {
-			var decoded = jwtDecode(jwt);
-			localStorage.setItem('type', Object.values(decoded)[1]);
-		}
-	}, []);
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<Login />} />
-				{localStorage.getItem('type') !== 'none' && (
-					<Route path='welcome' element={<Welcome />} />
-				)}
-				{/* <Route path='jobs' element={<Jobs />} /> */}
-			</Routes>
-		</BrowserRouter>
-	);
+  useEffect(() => {
+    var jwt = localStorage.getItem('jwt');
+    if (jwt !== null) {
+      var decoded = jwtDecode(jwt);
+      localStorage.setItem('type', Object.values(decoded)[1]);
+    }
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        {localStorage.getItem('type') !== 'none' && (
+          <Route path='welcome' element={<Welcome />} />
+        )}
+        {/* <Route path='jobs' element={<Jobs />} /> */}
+        <Route path='ChangeModelOnJob' element={<ChangeModelOnJob />} />
+        <Route path='CreateJob' element={<CreateJob />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
