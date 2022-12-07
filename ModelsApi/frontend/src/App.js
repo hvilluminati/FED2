c4f47a8652049358e830922198645065072ac99e
hvilluminati <Hvillum.magnus@gmail.com>
2022-12-07 14:46:03 +0100

    oof

--- ModelsApi/frontend/src/App.js
+++ ModelsApi/frontend/src/App.js
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Login from './pages/loginPage';
 import Welcome from './pages/welcomePage';
 import Jobs from './pages/jobListPage';
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
+import { setBaseUrl } from './Services/api';
+import { AddManager } from './pages/addManager';
 import jwtDecode from 'jwt-decode';
 
 function App() {
+	setBaseUrl('https://localhost:7181/api/');
+
 	const [type, setType] = useState('none');
 
 	useEffect(() => {
@@ -21,7 +25,8 @@ function App() {
 			<Routes>
 				<Route path='/' element={<Login func={setType} />} />
 				<Route path='welcome' element={<Welcome />} />
-				{/* <Route path='jobs' element={<Jobs />} /> */}
+				<Route path='jobs' element={<Jobs />} />
+				<Route path="/addmanager" element={<AddManager/>}/>
 			</Routes>
 		</BrowserRouter>
 	);
