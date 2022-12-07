85867998c3ec169a36dadeae6edcda040385c849
Nadixo <75481929+Nadixo@users.noreply.github.com>
2022-12-07 13:50:12 +0100

    add usestate

--- ModelsApi/frontend/src/pages/jobListPage.jsx
+++ ModelsApi/frontend/src/pages/jobListPage.jsx
@@ -1,23 +1,43 @@
-﻿
-<script>
-	const list = [{job: '', expense: 1, id: 1 }, {job: '', expense: 1, id: 1 }, {job: '', expense: 1, id: 1 }];
-	let Len = list.length;
+﻿import { useState, useEffect } from 'react';
+import { axiosCreateExpense } from '../axioscalls';
 
-	let text = "<ul>";
-		for (let i = 0; i < Len; i++) {
-			text += "<li>" + list[i] + "</li>";
-}
-		text += "</ul>";
-</script>
-class expenseBtn extends React.Component{ 
 
-	handleClick = () => {
-		const elem = document.getElementById('expense').value;
-		list.push(elem.ToString());
-	}
-};
+//<script>
+//	const list = [{job: '', expense: 1, id: 1 }, {job: '', expense: 1, id: 1 }, {job: '', expense: 1, id: 1 }];
+//	let Len = list.length;
+
+//	let text = "<ul>";
+//		for (let i = 0; i < Len; i++) {
+//			text += "<li>" + list[i] + "</li>";
+//}
+//		text += "</ul>";
+//</script>
+
+
 
 export default function JobList() {
+	const [expense, setExpense] = useState();
+	const [job, setJob] = useState();
+	const [jobID, setJobID] = useState();
+	const [modelID, setModelID] = useState();
+	const [date, setDate] = useState();
+	
+
+	function handleClick() {
+			var expenseList = [{}];
+			expense = document.getElementById('expense').value;
+			expenseList.push(expense);
+
+			date = `${current.getDate()}/${current.getMonth() + 1}/${current.getFullYear()}`;
+
+
+			axiosCreateExpense(modelID, jobID, date, job, expense).then((resp) => {
+				localStorage.setItem(resp.data.key, resp.data.value);
+			});
+	};
+	
+
+
 	return (
 		<div id='jobListPage'>
 			<h1>Job List</h1>
@@ -27,9 +47,10 @@ export default function JobList() {
 			</ul>
 			<div id='expenseTitle' className='jobPage' column='1'> Expenses </div>
 			<ul>
-				{list.map((item, i) => { <li key={i}> {item} </li> })}
+				{list.map((list, i) => { <li key={i}> {list} </li> })}
 			</ul>
-			<textarea id='expense' className='expenseText' rows='1' value='Add expense'> Add expense</textarea>
+			<textarea id='expense' className='expenseText' rows='1' > Add expense</textarea>
+			<textarea id='job' className='jobText' rows='1' > Add job you want to add expense to</textarea>
 			<button onClick={handleClick} id='expenseBtn'>Add expense</button>
 		</div>
 
